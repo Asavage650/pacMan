fc33a8cf63f186421f7a129788d32933d55a61f0
{alexius <{alexiussavage0@gmail.com}>
2023-08-24 13:52:46 -0700

    connecting db3

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/"));
+  res.sendFile(path.join(__dirname, "../client/build/"));
 });
 
 const startApolloServer = async () => {
