3155cdf7688eed1d694547946424713f8c72f243
{alexius <{alexiussavage0@gmail.com}>
2023-08-24 13:42:51 -0700

    connecting db

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 // const connectDB = async () => {
 const conn = mongoose.connect(
   process.env.MONGO_URI || "mongodb://localhost:27017/googlebooks"
